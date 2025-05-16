36be69661dec60f98b4f5e85fff23aa1870384cc
adnaneIIIII <adnane.elotmani@usmba.ac.ma>
2025-05-16 09:40:42 +0100

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "existing-domain.com",
+      "us-east-1-shared-usea1-02.graphassets.com",
+    ],
+  },
+};
+
+export default nextConfig;
